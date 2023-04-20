70174355e9826efef4f4c4e318e2ff798bae6d23
doaa <me@me.com>
2023-04-20 01:18:01 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 #include<stdio.h>
 /**
- * main - Entry point
- *
+ * main -  entry point
  * Return: Always 0 (Success)
- */
+ *Hello c
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
-
