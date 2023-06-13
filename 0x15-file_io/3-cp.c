3c8d07eb1e48fbaf6cf831dd7d75a2b8d43fe4c1
Doaa21 <doaaelhaweet2121@gmail.com>
2023-06-12 23:53:10 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,9 +43,9 @@ exit(100);
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ *If file_from does not exist or cannot be read - exit code 98.
+ *If file_to cannot be created or written to - exit code 99.
+ *If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
